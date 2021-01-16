c2edf05a60759d4e3d5fbf1c070c795366c912b4
Micah Kemp <apple@micah.ws>
2021-01-16 14:21:36 -0600

    Moved from gworkspace/ to internal/provider/.

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/micahkemp/terraform-provider-gworkspace/gworkspace"
+	"github.com/micahkemp/terraform-provider-gworkspace/internal/provider"
 )
 
 func main() {
 	opts := &plugin.ServeOpts{
-		ProviderFunc: gworkspace.Provider,
+		ProviderFunc: provider.Provider,
 	}
 
 	plugin.Serve(opts)

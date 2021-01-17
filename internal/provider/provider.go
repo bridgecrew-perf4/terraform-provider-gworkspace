d9d44653bd30e1e82c885884259ae4dac0cae31e
Micah Kemp <apple@micah.ws>
2021-01-16 21:45:51 -0600

    Re-start with mostly what is shown in the terraform-provider-scaffolding code.

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,22 +1,34 @@
 package provider
 
-import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+import (
+	"context"
 
-func Provider() *schema.Provider {
-	provider := &schema.Provider{
-		Schema: map[string]*schema.Schema{
-			"credential_file": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "Path to JSON-formatted Service Account credentials file",
-			},
-			"impersonate_email": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "Email address of the account to impersonate",
-			},
-		},
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+type apiClient struct {
+	// Add whatever fields, client or connection info, etc. here
+	// you would need to setup to communicate with the upstream
+	// API.
+}
+
+func configure(version string, p *schema.Provider) func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	return func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
+		// Setup a User-Agent for your API client (replace the provider name for yours):
+		// userAgent := p.UserAgent("terraform-provider-scaffolding", version)
+		// TODO: myClient.UserAgent = userAgent
+
+		return &apiClient{}, nil
 	}
+}
 
-	return provider
+func New(version string) func() *schema.Provider {
+	return func() *schema.Provider {
+		p := &schema.Provider{}
+
+		p.ConfigureContextFunc = configure(version, p)
+
+		return p
+	}
 }
